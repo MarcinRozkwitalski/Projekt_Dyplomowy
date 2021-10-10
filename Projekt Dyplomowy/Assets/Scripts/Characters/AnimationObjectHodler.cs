7b0b5838c629b3e318f0f2af99b14c36ec8f5d6f
TrojanFi <s20088@pjwstk.edu.pl>
2021-10-10 14:00:48 +0200

    Optimization

--- Projekt Dyplomowy/Assets/Scripts/Characters/AnimationObjectHodler.cs
+++ Projekt Dyplomowy/Assets/Scripts/Characters/AnimationObjectHodler.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AnimationObjectHodler : MonoBehaviour
 {
     void Start()
-    {
-        if(SentenceHandler.hashTableAnswers[int.Parse(gameObject.name)] == null){
+    { 
+        if(SentenceHandler.hashTableAnswers[int.Parse(gameObject.name)] == null || AnswerHandler.index == int.Parse(gameObject.name)){
             GetComponent<Renderer>().enabled = !GetComponent<Renderer>().enabled;
         }
     }
