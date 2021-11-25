31c19ca09002ba55c5aee3563049c29d75050676
Marcin <marcinrozkwitalski98@gmail.com>
2021-11-25 15:26:37 +0100

    Fixed animation handling (with Filip)
    TriggerAnimation.cs:
    - we trigger animations with doorStatus which is controlled by mouse clicks
    - Player can't move when animation is triggered
    - all animations must have "DefaultState"
    - all animators parameter "isIndexCorrect"
    - Parent must be the same name as Statement number
    
    PS. We need to handle when to turn parameter "isIndexCorrect" to false state and bool "PlayerMovement.canMove" to true state.
    
    PSS. DoorHandler.doorStatus to generated index

--- Projekt Dyplomowy/Assets/Scripts/Animations/TriggerAnimation.cs
+++ Projekt Dyplomowy/Assets/Scripts/Animations/TriggerAnimation.cs
@@ -2,33 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//sources
+//https://stackoverflow.com/questions/40752083/how-to-find-child-of-a-gameobject-or-the-script-attached-to-child-gameobject-via
+
 public class TriggerAnimation : MonoBehaviour
 {
 
+    Animator animator;
+
     void Start()
     {
-        if (AnswerHandler.index != int.Parse(gameObject.name))
-        {
-            GetComponent<Renderer>().enabled = !GetComponent<Renderer>().enabled; // nie rederuje animacji
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //(AnswerHandler.index != int.Parse(gameObject.name))
+        //podmienić to na dole :)
+        if (DoorHandler.doorStatus != int.Parse(gameObject.name))
+        {          
             GameObject originalGameObject = GameObject.Find(gameObject.name);
-            //https://stackoverflow.com/questions/40752083/how-to-find-child-of-a-gameobject-or-the-script-attached-to-child-gameobject-via
+            if(originalGameObject.GetComponent<Renderer>().enabled) originalGameObject.GetComponent<Renderer>().enabled = !originalGameObject.GetComponent<Renderer>().enabled;
+
             for (int i = 0; i < originalGameObject.transform.childCount; i++)
             {
                 GameObject child = originalGameObject.transform.GetChild(i).gameObject; // dziecko
-               // Debug.Log("Rodzic " + originalGameObject.name + " Dziecko " + child.name);
-                child.SetActive(false); // dzieki marcin 
+                //Debug.Log("Rodzic " + originalGameObject.name + " Dziecko " + child.name);
+                child.SetActive(false);
             }
 
         }
         else
         {
+            GameObject originalGameObject = GameObject.Find(gameObject.name);
+            if(originalGameObject.GetComponent<Renderer>().enabled == false) originalGameObject.GetComponent<Renderer>().enabled = !originalGameObject.GetComponent<Renderer>().enabled;
+
+            for (int i = 0; i < originalGameObject.transform.childCount; i++)
+            {
+                GameObject child = originalGameObject.transform.GetChild(i).gameObject; // dziecko
+                //Debug.Log("Rodzic " + originalGameObject.name + " Dziecko " + child.name);
+                child.SetActive(true);
+            }
             StartCoroutine(Time());
         }
     }
 
     IEnumerator Time()
     {
-        yield return new WaitForSeconds(3f);
-        GetComponent<Animator>().Play(gameObject.name);
+        yield return new WaitForSeconds(0f);
+        animator.SetBool("isIndexCorrect", true);
+        //GetComponent<Animator>().Play(gameObject.name);
+        PlayerMovement.canMove = false;
     }
 }
