63c7a6e6f9be3f82bcd476c4e5e05edee2c91613
TrojanFi <filip23t@gmail.com>
2021-12-25 12:24:37 +0100

    Added Script PlayerPathFollower
    in this script we will have prepared path for player to move in specific statements

---
+++ Projekt Dyplomowy/Assets/Scripts/Player/PlayerPathFollower.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPathFollower : MonoBehaviour
+{
+    
+    public void Statement_1_Active(){
+        
+    }
+
+
+
+
+}
